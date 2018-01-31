fd4c9bd80e162144c4c2cc0ba44468d95af223af
gnd <gnd@itchybit.org>
2018-01-31 10:07:53 +0100

    update

--- run-spider.sh
+++ run-spider.sh
@@ -7,5 +7,14 @@ if [[ -z "$1" ]]; then
 	exit
 fi
 
+if [[ "$2" == "tor" ]]; then
+	echo "Using tor"
+	TOR=1
+fi
+
 ### in order to make mrtg happy we need to output 4 values
-scrapy runspider $DATA_DIR"/"$1.py --nolog -o - -t csv
+if [[ $TOR == 1 ]]; then
+	torify scrapy runspider $SPIDER_DIR"/"$1.py --nolog -o - -t csv
+else
+	scrapy runspider $SPIDER_DIR"/"$1.py --nolog -o - -t csv
+fi

4b346a2a7702f49b23aeb8278786a0eaee2c1008
gnd <gnd@itchybit.org>
2018-02-19 22:03:53 +0100

    helper script

---
+++ fixer-def.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# # -*- coding: utf-8 -*-
+
+""" Smogdance
+
+    An open-source collection of scripts to collect, store and graph air quality data
+    from publicly available sources.
+
+    This polls SQL for the latest values of all sensors from a given city.
+
+    gnd, 2017 - 2018
+"""
+
+import os
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+import MySQLdb
+import ConfigParser
+
+### load config
+settings_file = os.path.join(sys.path[0], 'settings_python')
+config = ConfigParser.ConfigParser()
+config.readfp(open(settings_file))
+
+all_substances = ["co","no2","o3","pm10","pm25","so2"]
+
+### connect to the db
+DB_HOST = config.get('database', 'DB_HOST')
+DB_USER = config.get('database', 'DB_USER')
+DB_PASS = config.get('database', 'DB_PASS')
+DB_NAME = config.get('database', 'DB_NAME')
+DB_TABLE = config.get('database', 'DB_TABLE')
+DATA_TABLE = config.get('database', 'DATA_TABLE')
+db = MySQLdb.connect(host=DB_HOST, user=DB_USER, passwd=DB_PASS, db=DB_NAME, charset='utf8')
+cur = db.cursor()
+
+# get all CZ sensors
+query = "SELECT name, link_src, link_web, link_stat, link_xpaths, country, city, gps, type, substances FROM %s WHERE country = 'cz' ORDER BY id" % (DB_TABLE)
+try:
+    cur.execute(query)
+except:
+    sys.exit("Something went wrong: " + query)
+
+for l in cur.fetchall():
+    print "'%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s'" % (l[0],l[1].replace('/data/www/smog.dance/smogdance/temp','TEMP_DIR'),l[2],l[3].replace('stats.smog.dance','STATS_URL'),l[4],l[5],l[6],l[7],l[8],l[9])

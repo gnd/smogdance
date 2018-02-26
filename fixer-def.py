bf22a0ab92daca6be03de01c65cb502ea2ce31e4
gnd <gnd@itchybit.org>
2018-02-26 17:23:18 +0100

    fixing at

--- fixer-def.py
+++ fixer-def.py
@@ -35,8 +35,8 @@ DATA_TABLE = config.get('database', 'DATA_TABLE')
 db = MySQLdb.connect(host=DB_HOST, user=DB_USER, passwd=DB_PASS, db=DB_NAME, charset='utf8')
 cur = db.cursor()
 
-# get all CZ sensors
-query = "SELECT name, link_src, link_web, link_stat, link_xpaths, country, city, gps, type, substances FROM %s WHERE country = 'cz' ORDER BY id" % (DB_TABLE)
+# get all AT sensors  -- fixing AT - 26.2.2018
+query = "SELECT name, link_src, link_web, link_stat, link_xpaths, country, city, gps, type, substances FROM %s WHERE country = 'at' ORDER BY id" % (DB_TABLE)
 try:
     cur.execute(query)
 except:

37a6e71343423796cad381e65d624f7179573380
gnd <gnd@itchybit.org>
2020-03-17 15:25:52 +0100

    first version of sensor deletion

--- delete-sensor.py
+++ delete-sensor.py
@@ -6,16 +6,17 @@
     An open-source collection of scripts to collect, store and graph air quality data
     from publicly available sources.
 
-    (Skeleton) This removes a sensor from disk and database:
-        - moves the sensor record from sensors to sensors_removed in the db TODO
-        - moves all historical data associated with the sensor from sensors_data to sensors_data_deleted TODO
-        - deletes sensor spiderfile from the disk TODO
-        - recreates the city's sensor local id's (or deletes the city spider dir if only sensor) TODO
+    This removes a sensor from disk and database:
+        - moves the sensor record from sensors to sensors_deleted in the db
+        - moves all historical data associated with the sensor from sensors_data to sensors_data_deleted
+        - deletes sensor spiderfile from the disk
+        - recreates the city's sensor local id's (or deletes the city spider dir if only sensor)
         - recreates the city's mrtg directory (or deletes it if only sensor) TODO
 
-    gnd, 2017 - 2018
+    gnd, 2017 - 2020
 """
 
+import re
 import os
 import sys
 import time
@@ -30,6 +31,7 @@ settings_file = os.path.join(sys.path[0], 'settings_python')
 config = ConfigParser.ConfigParser()
 config.readfp(open(settings_file))
 STATS_DIR = config.get('globals', 'STATS_DIR')
+STATS_DIR_DEL = config.get('globals', 'STATS_DIR')
 
 ### connect to the db
 DB_HOST = config.get('database', 'DB_HOST')
@@ -37,7 +39,9 @@ DB_USER = config.get('database', 'DB_USER')
 DB_PASS = config.get('database', 'DB_PASS')
 DB_NAME = config.get('database', 'DB_NAME')
 DB_TABLE = config.get('database', 'DB_TABLE')
+DB_TABLE_DEL = config.get('database', 'DB_TABLE_DEL')
 DATA_TABLE = config.get('database', 'DATA_TABLE')
+DATA_TABLE_DEL = config.get('database', 'DATA_TABLE_DEL')
 db = MySQLdb.connect(host=DB_HOST, user=DB_USER, passwd=DB_PASS, db=DB_NAME)
 cur = db.cursor()
 
@@ -59,28 +63,280 @@ for line in cur.fetchall():
     city = line[2]
     country = line[3]
     substances = line[4]
+    city_dir = city.replace(" ", "_")
 
 #####
-##### Move the db data TODO
+##### Move sensor definition into sensors_deleted
 #####
-
-#####
-##### Delete spider file from disk TODO
-#####
-
-#####
-##### Recreate local ids, spider files and mrtg TODO
-#####
-
-#####
-##### Recreate mrtg stats for city TODO
-#####
+query = "INSERT INTO %s SELECT * FROM %s WHERE id = '%d'" % (DB_TABLE_DEL, DB_TABLE, sensor_id)
+try:
+    cur.execute(query)
+except:
+    sys.exit("Something went wrong: " + query)
+db.commit()
 
 
+#####
+##### Delete sensor definition from sensors
+#####
+query = "DELETE FROM %s WHERE id = '%d'" % (DB_TABLE, sensor_id)
+try:
+    cur.execute(query)
+except:
+    sys.exit("Something went wrong: " + query)
+db.commit()
 
-    sensor_id = row[0]
-    sensor_local_id = row[1]
-    sensor_country = row[2]
-    sensor_city = row[3]
-    sensor_substances = row[4].split()
-    print "Repopulating %s-%d: " % (sensor_city, sensor_local_id)
+
+#####
+##### Move the sensor data into sensor_data_deleted
+#####
+query = "INSERT INTO %s SELECT * FROM %s WHERE sensor_id = '%d'" % (DATA_TABLE_DEL, DATA_TABLE, sensor_id)
+try:
+    cur.execute(query)
+except:
+    sys.exit("Something went wrong: " + query)
+db.commit()
+
+
+#####
+##### Delete the sensor data
+#####
+query = "DELETE FROM %s WHERE sensor_id = '%d'" % (DATA_TABLE, sensor_id)
+try:
+    cur.execute(query)
+except:
+    sys.exit("Something went wrong: " + query)
+db.commit()
+
+
+#####
+##### Delete spider file from disk
+#####
+spider_file = "%s/%s/%s/%s.py" % (SPIDER_DIR, country, city_dir, local_id)
+print "Removing spider file from disk"
+try:
+    os.remove(spider_file)
+except:
+    sys.exit("Something went wrong when removing: " + spider_file)
+
+
+#####
+##### Recreate or delete local ids, spider files
+#####
+# Determine how many sensors from the city are left
+query = "SELECT count(local_id) FROM %s WHERE city = '%s'" % (DB_TABLE, city)
+try:
+    cur.execute(query)
+except:
+    sys.exit("Something went wrong: " + query)
+res = cur.fetchone()
+sensor_count = res[0]
+
+# If this wasnt the last sensor, renumber the leftover sensors
+if (sensor_count > 0):
+    id_mapping = {}
+    city_substances = []
+    new_local_id = 0
+    query = "SELECT id, local_id, substances FROM %s WHERE city = '%s' ORDER BY local_id" % (DB_TABLE, city)
+    try:
+        cur.execute(query)
+    except:
+        sys.exit("Something went wrong: " + query)
+    for line in cur.fetchall():
+        temp_sensor_id = line[0]
+        old_local_id = line[1]
+        temp_substances = line[2]
+        query = "UPDATE %s SET local_id = %d WHERE id = '%d'" % (DB_TABLE, new_local_id, temp_sensor_id)
+        db.commit()
+
+        # Create a list of all city substances to be used later
+        temp_substances_arr = temp_substances.split()
+        for temp_substance in temp_substances_arr:
+            if temp_substance not in city_substances:
+                city_substances.append(temp_substance)
+
+        # Delete old spiderfiles
+        sensor_city_dir = "%s/%s/%s" % (SPIDER_DIR, country, city_dir)
+        files = [f for f in os.listdir(sensor_city_dir) if os.path.isfile(os.path.join(sensor_city_dir, f))]
+        for file in files:
+            if ((".py" in file) or (".pyc" in file):
+                os.remove(file)
+
+        # Regenerate spiderfiles
+        query = "SELECT local_id, name, link_src, link_xpaths FROM %s WHERE city = '%s' AND type ='hourly' ORDER BY id" % (DB_TABLE, city)
+        try:
+            cur.execute(query)
+        except:
+            sys.exit("Something went wrong: " + query)
+        ### create new sensor spider on disk
+        for line in cur.fetchall():
+            temp_local_id = line[0]
+            temp_name = line[1]
+            temp_link_src = line[2]
+            temp_link_xpaths = line[3]
+            temp_spider_file = "%s/%s/%s/%s.py" % (SPIDER_DIR, country, city_dir, temp_local_id)
+            template = fill_spider_template(TEMP_DIR, SPIDER_TEMPLATE, temp_name, temp_link_src, temp_link_xpaths)
+            write_template(temp_spider_file, template)
+
+        # store mapping
+        id_mapping[old_local_id] = new_local_id
+        new_local_id++
+
+# If this was the last sensor, delete the directory and all its contents
+else:
+    sensor_city_dir = "%s/%s/%s" % (SPIDER_DIR, country, city_dir)
+    files = [f for f in os.listdir(sensor_city_dir) if os.path.isfile(os.path.join(sensor_city_dir, f))]
+    for file in files:
+        os.remove(file)
+    os.rmdir(sensor_city_dir)
+
+
+#####
+##### Regenerate mrtg config files with new local ids
+#####
+if ((sensor_count > 0) and (local_id < sensor_count-1)):
+    # Delete old mrtg config files
+    for substance in city_substances.split():
+        mrtg_name = "%s.cfg" % (substance)
+        mrtg_file = "%s/%s/%s/%s" % (SPIDER_DIR, country, city_dir, mrtg_name)
+        if os.path.isfile(mrtg_file):
+            os.remove(file)
+
+    # Now create new mrtg config files
+    query = "SELECT id, local_id, name, substances FROM %s WHERE city = '%s' AND type ='hourly' ORDER BY id" % (DB_TABLE, city)
+    try:
+        cur.execute(query)
+    except:
+        sys.exit("Something went wrong: " + query)
+    ### create new sensor spider on disk
+    for line in cur.fetchall():
+        temp_sensor_id = line[0]
+        temp_local_id = line[1]
+        temp_name = line[2]
+        temp_substances = line[3]
+        for temp_substance in temp_substances.split():
+            mrtg_name = "%s.cfg" % (substance)
+            mrtg_workdir = "%s/%s/%s/" %(STATS_DIR, country, city_dir)
+            mrtg_file = "%s/%s/%s/%s" % (SPIDER_DIR, country, city_dir, mrtg_name)
+            template = fill_mrtg_template(DATA_DIR, SPIDER_COMMAND, MRTG_TEMPLATE, temp_id, temp_local_id, temp_name, city, country, temp_substance)
+            write_mrtg_template(mrtg_file, mrtg_workdir, template)
+
+#####
+##### Remove city from mrtg runlist if last sensor
+#####
+if (sensor_count == 0):
+    if (os.path.isfile("%s/%s" % (DATA_DIR, 'mrtg.runlist'))):
+        # Read runlist
+        f = file("%s/%s" % (DATA_DIR, 'mrtg.runlist'), 'r')
+        lines = f.readlines()
+        f.close()
+
+        # Write new runlist
+        f = file("%s/%s" % (DATA_DIR, 'mrtg.runlist'), 'w')
+        for line in lines:
+            runlist_name = "%s/%s\n" % (country, city_dir)
+            if (runlist_name not in line):
+                f.write()
+        f.close()
+
+#####
+##### Move mrtg data for deleted sensor
+#####
+# prepare directories
+if (not os.path.isdir("%s" % (STATS_DIR_DEL))):
+    os.makedirs("%s" % (STATS_DIR_DEL))
+if (not os.path.isdir("%s/%s" % (STATS_DIR_DEL, country))):
+    os.makedirs("%s/%s" % (STATS_DIR_DEL, country))
+if (not os.path.isdir("%s/%s/%s" % (STATS_DIR_DEL, country, city_dir))):
+    os.makedirs("%s/%s/%s" % (STATS_DIR_DEL, country, city_dir))
+
+# Now move all files called like city-local_id*
+for sub in substances:
+    old_dir = "%s/%s/%s" % (STATS_DIR, country, city_dir)
+    new_dir = "%s/%s/%s" % (STATS_DIR_DEL, country, city_dir)
+    pattern = "%s-%s_%s" % (city, local_id, sub)
+    files = [f for f in os.listdir(old_dir) if os.path.isfile(os.path.join(old_dir, f))]
+    for file in files:
+        if (pattern in file):
+            print "Moving %s" % (file)
+            os.rename("%s/%s" % (old_dir, file), "%s/%s" % (new_dir, file))
+
+
+#####
+##### Rename mrtg data for leftover sensors
+#####
+if ((sensor_count > 0) and (local_id < sensor_count-1)):
+    old_dir = "%s/%s/%s" % (STATS_DIR, country, city_dir)
+    if (sensor_count > 0):
+        for former_id, new_id in id_mapping.iteritems():
+            pattern = "%s-%s.*\.(png|log|old)" % (city, former_id)
+            files = [f for f in os.listdir(old_dir) if os.path.isfile(os.path.join(old_dir, f))]
+            for file in files:
+                if re.match(pattern, file):
+                    name_root = "%s-%s" % (city, new_id)
+                    name_tail = file.split('_')[1]
+                    new_name = "%s_%s" % (name_root, name_tail)
+                    print "Renaming %s to %s" (file, new_name)
+                    os.rename("%s/%s" % (old_dir, file), "%s/%s" % (old_dir, new_name))
+
+
+#####
+##### Recreate mrtg stats for city
+#####
+### create index with indexmaker
+if (sensor_count > 0):
+    mrtg_statdir = "%s/%s/%s/" %(STATS_DIR, country, city_dir)
+    # move mrtg data for old sensor
+    for file in os.listdir(mrtg_statdir):
+        if file(".html"):
+            os.remove("%s/%s" % (mrtg_statdir, file))
+
+    # remove old index files
+    for file in os.listdir(mrtg_statdir):
+        if file.endswith(".html"):
+            os.remove("%s/%s" % (mrtg_statdir, file))
+
+    # Create new index files
+    for substance in city_substances:
+        links = ""
+        index_sub = 'pm10'
+        if 'pm10' in city_substances:
+            index_sub = 'pm10'
+        elif 'pm25' in city_substances:
+            index_sub = 'pm25'
+        elif 'co' in city_substances:
+            index_sub = 'co'
+        elif 'o3' in city_substances:
+            index_sub = 'o3'
+        for sub in city_substances:
+            if (sub != substance):
+                if (sub == index_sub):
+                    links += "<a href=index.html>%s</a> " % (sub.upper())
+                else:
+                    links += "<a href=%s.html>%s</a> " % (sub, sub.upper())
+
+        if (substance == index_sub):
+            command = "indexmaker --output=%s/%s/%s/%s.html --title=\"%s %s Levels (%s)\" --nolegend %s/%s/%s/%s.cfg" % (STATS_DIR, country, city_dir, "index", city.capitalize(), substance.upper(), links, SPIDER_DIR, country, city_dir, substance)
+        else:
+            command = "indexmaker --output=%s/%s/%s/%s.html --title=\"%s %s Levels (%s)\" --nolegend %s/%s/%s/%s.cfg" % (STATS_DIR, country, city_dir, substance, city.capitalize(), substance.upper(), links, SPIDER_DIR, country, city_dir, substance)
+
+        command_args = shlex.split(command)
+        ### run indexmaker
+        try:
+            process = subprocess.Popen(command_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out = process.communicate()
+        except subprocess.CalledProcessError as e:
+            print e
+            out = "UGH"
+            pass
+
+        ### if non-zero return, we have a problem
+        if (out[1] != ""):
+            print "Something went wrong: %s" % (out[1])
+        else:
+            print "mrtg index for %s created" % (substance)
+
+#####
+##### Done
+#####
+print "Sensor %s from %s with local_id %d and id %d deleted." % (name, city, local_id, id)
